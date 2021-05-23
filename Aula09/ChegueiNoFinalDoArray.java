bd556c221eb827148e270d29157c2c7d07589311
NULLBYTE-RGH <79452652+NULLBYTE-RGH@users.noreply.github.com>
2021-05-23 18:37:06 -0300

    Add files via upload

---
+++ Aula09/ChegueiNoFinalDoArray.java
@@ -0,0 +1,9 @@
+//UNchecked
+public class ChegueiNoFinalDoArray extends ArrayIndexOutOfBoundsException{
+//Checked
+//public class ChegueiNoFinalDoArray extends Exception{
+    @Override
+    public String getMessage() {
+        return "Array Chegou no final";
+    }
+}
