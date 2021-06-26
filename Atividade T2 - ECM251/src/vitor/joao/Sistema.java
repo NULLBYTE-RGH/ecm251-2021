dbb93621771ae153d5cfd724328fa2f99bdc847a
NULLBYTE-RGH <79452652+NULLBYTE-RGH@users.noreply.github.com>
2021-06-26 19:22:56 -0300

    Add files via upload

--- Atividade T2 - ECM251/src/vitor/joao/Sistema.java
+++ Atividade T2 - ECM251/src/vitor/joao/Sistema.java
@@ -14,26 +14,37 @@ public class Sistema{
         //opçoes e exibir horario
     }
 
+    public void Login(){
+
+    }
+
     public void Cadastro(){
-        String nome;
-        String email;
-        String funcao;
-        String hora;
+        //Os integrantes devem ser cadastrados por
+        //seus nomes de usuário, e-mail e função. O sistema deve ser capaz
+        //de mudar o horário de trabalho quando for solicitado.
 
         //Os integrantes devem ser cadastrados por
         //seus nomes de usuário, e-mail e função. O sistema deve ser capaz
         //de mudar o horário de trabalho quando for solicitado.
+
+        String nome;
+        String email;
+        String funcao;
+
     }
 
-    public String Horario(){
-
+    public String Mostar_Hora(){
         //Os membros também devem ser capazes de perguntarpara o sistema se ele está em horário normal de trabalho ou se
         //estão em horário de atividades “extras”
 
-        String hora ="3:40";
-
-        return hora;
+        String hora = "3:40";
+            return hora;
     }
+    public void Mudar_Hora(){
 
+        //O sistema deve ser capaz
+        //de mudar o horário de trabalho quando for solicitado.
+
+    }
 
 }
