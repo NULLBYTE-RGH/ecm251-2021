bc8ca738e501b1d3b8616c5c817ee54f1d44c333
NULLBYTE-RGH <79452652+NULLBYTE-RGH@users.noreply.github.com>
2021-06-27 17:18:18 -0300

    Add files via upload

---
+++ Atividade T2 - ECM251/Atividade T2 - ECM251/src/vitor/joao/interfaces/Apresentacao.java
@@ -0,0 +1,10 @@
+package vitor.joao.interfaces;
+
+//Quando for preciso, é preciso exibir um relatório contendo as
+//informações de cada um dos membros. Cada membro deve ser capaz
+//de implementar a interface Apresentacao (de novo ele foi muito
+//enfático na palavra INTERFACE), que deve permitir que ele se
+//apresente quando o relatório for chamado
+
+public interface Apresentacao {
+}
